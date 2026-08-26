65a89927b50224b60cc3e7c6bac7fd2610c6b3e2
unknown <german.scarafilo@bue.edu.ar>
2026-08-26 16:29:57 -0300

    Primera clase de objetos

---
+++ Objetos/Objetos/Automovil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objetos
+{
+    internal class Automovil
+    {
+        // Atributos
+        // marca, ruedaAuxilio, cantidadPuertas, tipoCombustible
+        public string marca;
+        public bool tieneRuedaAuxilio;
+        public int cantidadPuertas;
+        public string tipoCombustible;
+        public float kmRecorridos;
+        
+        // Metodos
+        // Desplazarse, Frenar, TocarBocina
+
+        public void MostrarAutomovil()
+        {
+            Console.WriteLine($"{marca} - {tieneRuedaAuxilio} - {cantidadPuertas} - {tipoCombustible} - {kmRecorridos}");
+        }
+
+
+    }
+}
---
+++ Objetos/Objetos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Automovil unAuto = new Automovil();
+
+            unAuto.tipoCombustible = "Diesel";
+            unAuto.cantidadPuertas = 4;
+            unAuto.tieneRuedaAuxilio = false;
+            unAuto.kmRecorridos = 50000;
+            unAuto.marca = "Ford Ka";
+
+
+            Automovil otroAuto = new Automovil();
+            otroAuto.tipoCombustible = "GNC";
+            otroAuto.cantidadPuertas = 2;
+            otroAuto.tieneRuedaAuxilio = true;
+            otroAuto.kmRecorridos = 20000;
+            otroAuto.marca = " Chevrolet lalal";
+
+            unAuto.MostrarAutomovil();
+            otroAuto.MostrarAutomovil();
+
+        }
+
+    }
+}
